7dba35acb8c1fc798c3eba111e2c4e70deef50f6
John-Anthony Thevos <john.thevos@gmail.com>
2022-06-24 08:01:17 -0400

    Add placeholder files for eventual python scripts

---
+++ analyze_comments.py
@@ -0,0 +1,8 @@
+import csv
+import pandas as pd
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ extract_comments.py
@@ -0,0 +1,16 @@
+import csv, json, requests
+
+API_KEY = ""
+
+def main():
+    # hit youtube API
+    # pull all comments for DS
+    # append to csv
+
+    # hit youtube API
+    # pull all comments for non DS
+    # append to csv
+    pass
+
+if __name__ == "__main__":
+    main()
